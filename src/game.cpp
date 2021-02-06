9ce942be1ad3b5fc1765d7a696ad10e5efdf7e07
palderson <paulderson@gmail.com>
2021-02-06 18:43:55 +0100

    New Office graphic.

--- src/game.cpp
+++ src/game.cpp
@@ -496,8 +496,12 @@ void Game::updateTitle(uint8_t frame) {
 }
 
 void Game::renderTitle(uint8_t frame) {
-    renderer.drawOverwrite(35, 11, titleText_tiles, 0);
+    renderer.drawOverwrite(35, 11, OfficeTextTitle_tiles, 0);
     // renderer.drawOverwrite(76, 7, titleKey_tiles, 0);
+    renderer.drawOverwrite(11, 10, AmsterdamX_tiles, 0);
+    renderer.drawOverwrite(11, 26, AmsterdamX_tiles, 0);
+    renderer.drawOverwrite(11, 42, AmsterdamX_tiles, 0);
+    
 
     const uint8_t* startGameLabel = State::hasUserSaved() ? continue_string : newGame_string;
 
